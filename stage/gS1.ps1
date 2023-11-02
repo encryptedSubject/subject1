90b15f55519eed46a6b9f2425a21fbb8f8c1882a
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2023-11-02 12:28:10 -0400

    Update gS1.ps1

--- stage/gS1.ps1
+++ stage/gS1.ps1
@@ -5,3 +5,5 @@ $set = @{
   Description = 'jeff admin acc'
 }
 New-LocalUser @set  
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
+Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
