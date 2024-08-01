ecdc10c3ad28b7bd25b1e4ef150c1f3e98c106c0
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2024-08-01 16:44:44 -0400

    Update gS1.ps1

--- stage/gS1.ps1
+++ stage/gS1.ps1
@@ -1,5 +1,5 @@
 $pw = ConvertTo-SecureString "mustbeSick!" -AsPlainText -Force
-$set = @{Name='jadmin',Password=$pw,Description='jeff admin acc'}
+$set = @{Name='jadmin'; Password=$pw; Description='jeff admin acc'}
 New-LocalUser @set
 Add-LocalGroupMember -Group "Administrators" -Member "jadmin"
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
