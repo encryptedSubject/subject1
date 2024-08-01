04d8eba8e2b50189a191c9edd5ddda4a976b249d
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2024-08-01 16:51:22 -0400

    Update gS1.ps1

--- stage/gS1.ps1
+++ stage/gS1.ps1
@@ -1,6 +1,9 @@
 $pw = ConvertTo-SecureString "mustbeSick!" -AsPlainText -Force
-$set = @{Name='jadmin'; Password=$pw; Description='jeff admin acc'}
-New-LocalUser $set
+$set = @{
+    Name='jadmin';
+    Password=$pw;
+    Description='jeff admin acc'}
+New-LocalUser @set
 Add-LocalGroupMember -Group "Administrators" -Member "jadmin"
 Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
