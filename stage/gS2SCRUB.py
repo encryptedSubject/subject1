67957746ce54356545b789fcfe17f2f80afb03f1
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2024-08-08 11:48:21 -0400

    Update gS2SCRUB.py

--- stage/gS2SCRUB.py
+++ stage/gS2SCRUB.py
@@ -1,15 +1,13 @@
 import os
 
-exts = ['.pdf', '.docx', '.ppt', '.txt', '.xls', '.png', '.jpg']
-
-
-#gets list of files for encryption
+#gets list of files for mod
 def start(userdir):
     for root, dirs, files in os.walk(userdir, topdown=True):
         for name in files:
             target = os.path.join(root,name)
             ext = os.path.splitext(name)
-            if ext[1] in exts and ext[0][0] != "#" and ext[0][0] != "$":
+            if ext[1] == '.ENSUIR':
+                print(f"[!] Decrypting {target}")
                 dencrypt(target)
 
 
@@ -19,22 +17,21 @@ def xord(data):
 
 #It will XOR the data from a file once with b^0xA8
 def dencrypt(file):
-    if os.path.splitext(file)[1] == ".ENSUIR":
-        try:
-            with open(file, 'rb') as f:
-                data = f.read()
-        except:
-            pass
-        try:
-            with open(file, 'wb') as fp:
-                fp.write(xord(data))
-            os.rename(file, os.path.splitext(file)[0])
-        except:
-            pass
+    try:
+        with open(file, 'rb') as f:
+            data = f.read()
+    except:
+        pass
+    try:
+        with open(file, 'wb') as fp:
+            fp.write(xord(data))
+        os.rename(file, os.path.splitext(file)[0])
+    except:
+        pass
 
 
 def main():
-    userdir = os.path.expanduser('~')
+    userdir = os.getcwd()
     start(userdir)
 
 if __name__ == "__main__":
